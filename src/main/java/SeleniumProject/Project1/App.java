6ae3c4589717e1237fb7c9501af97fa11632bf37
Sangeetha Thiagarajan <thiagarajansangeetha@gmail.com>
2026-01-10 00:21:46 +1100

    checkout some changes

--- src/main/java/SeleniumProject/Project1/App.java
+++ src/main/java/SeleniumProject/Project1/App.java
@@ -17,6 +17,7 @@ public class App
 {
     public static void main( String[] args )
     {
+    	 System.out.println( "HI ,Hello World , How r u !!!" );
         System.out.println( "HI ,Hello World , How r u !!!" );
         System.out.println( "HI ,Hello World , How r u !!!" );
         WebDriverManager.chromedriver().setup();
